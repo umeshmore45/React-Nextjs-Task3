438403b5f0afd89123828e2a3d89e0c252288335
Trainee 29 <umesh.more@raweng.com>
2021-01-10 21:15:43 +0530

    first commit

---
+++ pages/contentstack.js
@@ -0,0 +1,9 @@
+function ContentStack(props) {
+  return (
+    <div>
+      <h1>Content Stack </h1>
+    </div>
+  );
+}
+
+export default ContentStack;
---
+++ pages/raw.js
@@ -0,0 +1,9 @@
+function Raw(props) {
+  return (
+    <div>
+      <h1>Raw</h1>
+    </div>
+  );
+}
+
+export default Raw;
