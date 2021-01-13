6539e19853cf776cae021b789da4b465c75a5715
Trainee 29 <umesh.more@raweng.com>
2021-01-13 18:57:09 +0530

    fetch users data using sdk

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,45 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import SurfRawComponent from "../component/SurfRaw";
-import SurfContentComponent from "../component/SurfContent";
+import SurfComponent from "../component/SurfCom";
+
+import contentstack from "contentstack";
 
 export default function Home(props) {
   return (
     <div className={styles["container"]}>
       <h1>Surfboard</h1>
-      <SurfRawComponent />
-      <SurfContentComponent />
+      {props.employee.map((emp) => {
+        return <SurfComponent key={emp.uid} {...emp} />;
+      })}
     </div>
   );
 }
+
+export const getStaticProps = () => {
+  const Stack = contentstack.Stack(
+    process.env.API_KEY,
+    process.env.DELIVERY_TOKEN,
+    process.env.ENVIROMENT
+  );
+  const Query = Stack.ContentType("umesh_surfcom").Query();
+
+  return Query.where("title")
+    .includeSchema()
+    .includeCount()
+    .toJSON()
+    .find()
+    .then(
+      function success(result) {
+        return {
+          props: {
+            employee: [...result[0]],
+          },
+        };
+      },
+      function error(err) {
+        console.log(err);
+        return err;
+      }
+    );
+};
